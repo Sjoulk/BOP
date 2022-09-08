fb751205fb035cf326fac7b786df1ab916eb10b8
Sjoulk <shd@live.no>
2022-09-08 12:45:08 +0200

    new file:   BoP22/Arbeidsfiler/Covid19_sim.py

---
+++ BoP22/Arbeidsfiler/Covid19_sim.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as ani
+import numpy as np
+
+GREY = (0.78, 0.78, 0.78) # uninfected
+RED = (0.96, 0.15, 0.15)  # infected
+GREEN = (0, 0.86, 0.03)   # recovered
+BLACK = (0, 0, 0)         # dead
+
+COVID19_PARAMS = {
+    "r0": 2.28,
+    "incubation": 5,
+    "percent_mild": 0.8,
+    "mild_recovery": (7, 14),
+    "percent_severe": 0.2,
+    "severe_recovery": (21, 42),
+    "severe_death": (14, 56),
+    "fatality_rate": 0.034,
+    "serial_interval": 7
+}
+
+class Virus():
+    def __init__(self, params):
+        # create plot
+        self.fig = plt.figure()
+        self.axes = self.fig.add_subplot(111, projection="polar")
+        self.axes.grid(False)
+        self.axes.set_xticklables([])
+        self.axes.set_yticklables([])
+        self.axes.set_ylim(0, 1)
+
+        # create annotations
+        self.day_text = self.axes.annotate(
+            "Day 0", xy=[np.pi / 2, 1], ha="center", va="bottom"
+        )
+        self.infected_text = self.axes.annotate(
+            "Infected: 0", xy=[3 * np.pi / 2, 1], ha="center", va="top", color=RED
+        )
+        self.death_text = self.axes.annotate(
+            "\nDeaths: 0", xy=[3 * np.pi / 2, 1], ha="center", va="top", color=BLACK
+        )
+        self.recovered_text = self.axes.annotate(
+            "\n\nRecovered: 0", xy=[3 * np.pi / 2,1], ha="center", va="top", color=GREEN
+        )
+
+        #create member variables
+        self.day = 0
+        self.total_num_infected = 0
+        self.num_currently_infected = 0
+        self.num_recovered = 0
+        self.num_deaths = 0
+        self.r0 = params["r0"]
+        self.percent_mild = params["percent_mild"]
+        
